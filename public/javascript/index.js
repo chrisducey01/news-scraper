b8a9dcac7bfd46c633fe5c2d5b52f829ec1e7952
Christopher Ducey <chrisducey01@me.com>
2019-11-24 21:29:42 -0700

    Updated main page to reload after scraping new articles

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("#get-news-btn").click(event=>{
         //Scrape news by calling out to api and then displaying results
         $.get("/api/scrape").then(data=>{
-            console.log(data);
+            location.reload();
         });
     });
 });
